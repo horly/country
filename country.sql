c0c6c7bede9d64624b74aa5560295a34c3ca4303
Andelo Mata <andmatboy@gmail.com>
2023-09-05 15:24:56 +0100

    Update country.sql

--- country.sql
+++ country.sql
@@ -1,34 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : Dim 07 fév. 2021 à 13:18
--- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 8.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `symfony_kedisonline_dev`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `country`
---
-
 CREATE TABLE `countries` (
-  `id` int(10) NOT NULL,
+  `id` int(10) NOT NULL UNSIGNED,
   `code` int(10) NOT NULL,
   `alpha2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `alpha3` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
