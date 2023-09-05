221934fc8fbb8cc6e8624516f135346fc8d0acc7
Andelo Mata <andmatboy@gmail.com>
2023-09-05 10:47:19 +0100

    Update country.sql

--- country.sql
+++ country.sql
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 -- Structure de la table `country`
 --
 
-CREATE TABLE `country` (
+CREATE TABLE `countries` (
   `id` int(11) NOT NULL,
   `code` int(11) NOT NULL,
   `alpha2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
